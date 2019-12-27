b92c085a68bae6b9f462f1fcc6c687804e5d1bb7
Shivanku Kumar <shivanku.kumar@ITs-MacBook-Pro.local>
2019-12-27 05:17:20 -0800

    First draft

---
+++ server.js
@@ -0,0 +1,55 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, ()=> console.log(`Listening on port: ${port}`));
+
+app.get('/getVideoData', async (req, res) => {
+    let rawcsv = fs.readFileSync('./RawData/VideoData.csv', 'utf8')
+    let prevjson = JSON.parse(fs.readFileSync('./RawData/VideoData.JSON'))
+    let revisedJSON = await csvJSON(rawcsv, prevjson);
+    fs.writeFileSync('./RawData/VideoData.JSON', JSON.stringify(revisedJSON, null, 2))
+    res.send(revisedJSON)
+})
+
+function csvJSON(csv, prevjson){
+    return new Promise((resolve, reject) => {
+        var lines=csv.split(/\r?\n/);
+        lines.shift();
+        let prevTotalVideos = prevjson.totalVideos ? prevjson.totalVideos : 0 ;
+        if(prevTotalVideos === lines.length) {
+            resolve(prevjson)
+        }
+        else{
+            for(let i = prevTotalVideos ; i < lines.length ; i++) {
+                let currentline = lines[i].split(",");
+                if(prevjson.cities && prevjson.cities[currentline[1]]) {
+                    prevjson.cities[currentline[1]].videos.push(currentline[0])
+                }
+                else {
+                    if(!prevjson.cities) {
+                        prevjson.cities = {}
+                    }
+                    prevjson.cities[currentline[1]] = {
+                        videos: [currentline[0]],
+                        coordinates: {
+                            latitude: currentline[2],
+                            longitude: currentline[3]
+                        }
+                    }
+                }
+            }
+            prevjson.totalVideos = lines.length;
+            resolve(prevjson)
+        }
+        reject({
+            error: 'something went wrong in csv to JSON conversion'
+        })
+    })
+}
