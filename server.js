4051641131b3e0a17b6f6f401d977096b01b20fb
Shivanku Kumar <shivanku.kumar@ITs-MacBook-Pro.local>
2019-12-27 06:39:14 -0800

    changed package

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-      
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build/index.html'));
-    });
-  }
-
-app.listen(port, ()=> console.log(`Listening on port: ${port}`));
-
 app.get('/getVideoData', async (req, res) => {
     let rawcsv = fs.readFileSync('./RawData/VideoData.csv', 'utf8')
     let prevjson = JSON.parse(fs.readFileSync('./RawData/VideoData.JSON'))
@@ -64,3 +52,15 @@ function csvJSON(csv, prevjson){
         })
     })
 }
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    });
+  }
+
+app.listen(port, ()=> console.log(`Listening on port: ${port}`));
