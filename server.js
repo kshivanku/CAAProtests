f426e4f5500a91192337fd878d7e64957c44a400
Shivanku Kumar <shivanku.kumar@ITs-MacBook-Pro.local>
2019-12-27 05:47:36 -0800

    changed package

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 app.listen(port, ()=> console.log(`Listening on port: ${port}`));
 
 app.get('/getVideoData', async (req, res) => {
