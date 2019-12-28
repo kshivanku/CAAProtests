608795b771c8152434da5f2b7aa77f0932696cb8
Shivanku Kumar <kshivanku@gmail.com>
2019-12-27 22:38:55 -0800

    layout changes

--- client/src/CityDetailView.js
+++ client/src/CityDetailView.js
@@ -42,22 +42,25 @@ export function CityDetailView(props) {
                 initial = "close"
                 animate = "open"
                 exit = "close"
-            >   <div className="cityDetailView_Header">
-                    <h1>{selectedCity}</h1>
-                    <button onClick={(e) => onCityDetailClose(e)}>Close</button>
-                </div>
-                <div className="cityDetailView_videoList">
-                    {videoData[selectedCity].videos.map((link, index) => {
-                        if(link.indexOf('twitter') !== -1) {
-                            let id = link.split(/\/?\//)[4].split('?')[0];
-                            console.log(id)
-                            return <TwitterVideoEmbed id={id} key={id + index}/>
-                        }
-                        else {
-                            return <a href={link} key={index} target="_blank" style={{display: "block", marginBottom: '8px', color:'#fff'}}>{link} </a>
-                        }   
-                    })}
-                </div>
+            >   
+                {/* <div style = {{position: 'relative'}}> */}
+                    <div className="cityDetailView_Header">
+                        <h1>{selectedCity}</h1>
+                        <button onClick={(e) => onCityDetailClose(e)}>Close</button>
+                    </div>
+                    <div className="cityDetailView_videoList">
+                        {videoData[selectedCity].videos.map((link, index) => {
+                            if(link.indexOf('twitter') !== -1) {
+                                let id = link.split(/\/?\//)[4].split('?')[0];
+                                console.log(id)
+                                return <TwitterVideoEmbed id={id} key={id + index}/>
+                            }
+                            else {
+                                return <a href={link} key={index} target="_blank" style={{display: "block", marginBottom: '8px', color:'#fff'}}>{link} </a>
+                            }   
+                        })}
+                    </div>
+                {/* </div> */}
               
             </motion.div>
           )}
