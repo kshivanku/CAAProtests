e6df690d3e942d288a36f6338672ad76201ff16e
Shivanku Kumar <kshivanku@gmail.com>
2019-12-28 13:29:04 -0800

    css changes

--- client/src/CityDetailView.js
+++ client/src/CityDetailView.js
@@ -46,7 +46,7 @@ export function CityDetailView(props) {
                     {videoData[selectedCity].videos.map((link, index) => {
                         if(link.indexOf('twitter') !== -1) {
                             let id = link.split(/\/?\//)[4].split('?')[0];
-                            return <TwitterVideoEmbed id={id} key={id + index}/>
+                            return <TwitterVideoEmbed id={id} key={id + index} onLoad={e => e.style.display = "inline-block"} />
                         }
                         else {
                             return <a href={link} key={index} target="_blank" style={{display: "block", marginBottom: '8px', color:'#fff'}}>{link} </a>
