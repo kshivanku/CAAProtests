86b55c995ee502b551c8fe2d1129bf3cc4d53ce2
Aditya Dipankar <adityadipankar@gmail.com>
2019-12-31 22:38:48 +0530

    Typography mods to the landing screen

--- client/src/components/IntroScreen.js
+++ client/src/components/IntroScreen.js
@@ -10,12 +10,13 @@ export function IntroScreen() {
             className="introScreenContainer"
             animate={controls}
         >
-            <motion.div 
+            <motion.div
                 className="introScreenContent"
                 animate={contentControls}
             >
-                <h1>This is a visualization of all the protests against Citizenship Amendment Act (CAA) across India</h1>
-                <motion.button 
+                <h1>India has been on the streets this winter to save the secular fabric of the country</h1>
+                <h2>Over 20 civilians have died since these protests began on 20th of December 2019. Here is a map of the protests across the country.</h2>
+                <motion.button
                     className="primary_btn"
                     whileHover={{scale: 1.02}}
                     whileTap={{scale:0.9}}
@@ -23,7 +24,7 @@ export function IntroScreen() {
                         controls.start(close)
                         contentControls.start({opacity: 0, transition: {duration: 0.1}})
                     }}
-                >Lets go!
+                >Enter
                 </motion.button>
             </motion.div>
         </motion.div>
