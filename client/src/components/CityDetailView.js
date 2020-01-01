4150c4e2df8560f5c23411b9d66d5c8c498faff8
Shivanku Kumar <kshivanku@gmail.com>
2020-01-01 04:30:43 -0800

    Swipe to dismiss interaction on city details panel

--- client/src/components/CityDetailView.js
+++ client/src/components/CityDetailView.js
@@ -1,50 +1,101 @@
-import React from 'react';
-import {motion, AnimatePresence, useMotionValue} from 'framer-motion';
+import React, { useEffect, isValidElement } from 'react';
+import {motion, useMotionValue, useAnimation, AnimatePresence} from 'framer-motion';
 import {TwitterVideoEmbed} from 'react-twitter-embed';
 import close from '../icons/close.svg'
 
 export function CityDetailView(props) {
     const {selectedCity, videoData, onCityDetailClose, desktopSize } = props;
-    const y = useMotionValue(0);
+    let currentScrollValue = useMotionValue(0);
+    let prevScrollValue = useMotionValue(0);
+    let isDragging = false;
+    const containerAnimControls = useAnimation();
+    const headerAnimControls = useAnimation();
+    const bodyAnimControls = useAnimation();
 
-    const closeTransition = {ease: 'easeOut', duration: 0.2, staggerChildren: 0.05, when: "afterChildren", staggerDirection: -1}
-    const openTransition = {type: 'spring', damping: 16, staggerChildren: 0.1, delayChildren: 0.3}
+    const closeTransition = {ease: 'easeOut', duration: 0.2}
+    const openTransition = {type: 'spring', damping: 16}
 
     const variants = {
-        open: (window.innerWidth > desktopSize) ? {width: '30em', height: 'calc(var(--vh, 1vh) * 100)', overflowY: 'scroll', transition: openTransition}:{width: 'calc(var(--vw, 1vw) * 100)', height: 'calc(var(--vh, 1vh) * 90)', overflowY: 'scroll', transition: openTransition},
-        close: (window.innerWidth > desktopSize) ? {width: 0, height: 'calc(var(--vh, 1vh) * 100)', overflow: 'hidden', transition: closeTransition} : {width: 'calc(var(--vw, 1vw) * 100)', height: 0, overflow: 'hidden', transition: closeTransition}
+        open: (window.innerWidth > desktopSize) ? {width: '30em', top: 0, transition: openTransition}:{width: 'calc(var(--vw, 1vw) * 100)', top: 'calc(var(--vh, 1vh) * 20)', transition: openTransition},
+        close: (window.innerWidth > desktopSize) ? {width: 0, top: 0, y: 0, transition: closeTransition} : {width: 'calc(var(--vw, 1vw) * 100)', top: 'calc(var(--vh, 1vh) * 100)', y: 0, transition: closeTransition}
     }
 
     const childVariants = {
         open: {opacity: 1, y: 0, transition: {ease: 'easeOut'}}, close: {opacity: 0, y: 10, transition: {ease: 'linear'}}
     }
 
+    const scrollToTop = {top: 0, transition:{ease:'linear', duration: 0.2}}
+
+    const handleInOutAnimation = async () => {
+        if(selectedCity) {
+            containerAnimControls.start(variants.open)
+            await headerAnimControls.start(childVariants.open)
+            bodyAnimControls.start(childVariants.open)
+        }
+        else { 
+            await headerAnimControls.start(childVariants.close)
+            await bodyAnimControls.start(childVariants.close)
+            containerAnimControls.start(variants.close)
+        }
+    }
+
+    useEffect(()=> {
+        handleInOutAnimation()
+        window.addEventListener('touchmove', function() {isDragging = true;})
+        window.addEventListener('touchend', function(e) {
+            if(currentScrollValue.get() <= 0 && isDragging) {
+                onCityDetailClose();
+                isDragging = false;
+            }
+        });
+        // window.addEventListener('wheel', function() {
+        //     setTimeout(()=> {
+        //         if(currentScrollValue.get() === prevScrollValue.get()) {
+        //             onCityDetailClose();
+        //             prevScrollValue.set(0)
+        //         }
+        //         else {
+        //             prevScrollValue.set(currentScrollValue.get())
+        //         }
+        //     }, 500)
+        // })
+    }, [selectedCity])
+
     return (
         <AnimatePresence>
           {selectedCity && (
-            <motion.div 
-                y = {y}
+            <motion.div
                 className="cityDetailView"
-                variants = {variants}
-                initial = "close"
-                animate = "open"
-                exit = "close"
-                // drag = {(window.innerWidth > desktopSize) ? false : "y"}
-                // dragConstraints={{ top: 20, bottom: 20 }}
-                // dragElastic={0.2}
-                // onDragEnd = {() => {
-                //     if(y.get() < - 20) {
-
-                //     }
-                // }} 
+                variants={variants}
+                initial="close"
+                exit="close"
+                animate = {containerAnimControls}
+                onScroll = {(e) => {
+                    currentScrollValue.set(e.nativeEvent.target.scrollTop);
+                    if(currentScrollValue.get() > 0) {
+                        containerAnimControls.start(scrollToTop)
+                    }
+                }}
+                style={{height: 'calc(var(--vh, 1vh) * 100)', overflow: 'scroll', scrollBehavior: 'smooth', overscrollBehaviorY: 'none'}}
             >   
-                <motion.div className="cityDetailView_Header" variants={childVariants}>
+                <motion.div 
+                    className="cityDetailView_Header" 
+                    variants={childVariants}
+                    initial="close"
+                    animate = {headerAnimControls}
+                >
                     <h1>{selectedCity}</h1>
                     <button className="close_btn" onClick={(e) => onCityDetailClose(e)}>
                         <img src={close} alt="close icon" />
                     </button>
                 </motion.div>
-                <motion.div className="cityDetailView_videoList" variants={childVariants}>
+                <motion.div 
+                    className="cityDetailView_videoList" 
+                    variants={childVariants}
+                    initial="close"
+                    animate={bodyAnimControls}
+                    
+                >
                     {videoData[selectedCity].videos.map((videoObj, index) => {
                         if(videoObj.link.indexOf('twitter.com') !== -1) {
                             let id = videoObj.link.split(/\/?\//)[4].split('?')[0];
