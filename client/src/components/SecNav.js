66a2662fd9c5a024c8ee348185a67ee1c70425e4
Shivanku Kumar <kshivanku@gmail.com>
2020-01-03 16:16:04 -0800

    Added secondary nav with about and twitter

---
+++ client/src/components/SecNav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import info from '../icons/info.svg'
+import twitter from '../icons/twitter.svg'
+import {PageView, Event} from "./Tracking"
+
+export function SecNav(props) {
+    let {handleAboutClicked, desktopSize} = props
+    return (
+        <ul className="secNavContainer">
+            <li><a 
+                href="#" 
+                datacontent="About" 
+                style={{backgroundImage: `url(${info})`}} 
+                onClick={()=> {
+                    handleAboutClicked();
+                    PageView('About');
+            }}></a></li>
+            <li><a 
+                href="https://twitter.com/narendramodi" 
+                id="twitterNav" 
+                target="_blank" 
+                datacontent="Twitter" 
+                style={{backgroundImage: `url(${twitter})`}}
+                onClick = {() => Event("UserAction", "Navigated to Twitter", window.innerWidth > desktopSize ? "DESKTOP": "MOBILE")}
+            >
+            </a></li>
+        </ul>
+    )
+}
