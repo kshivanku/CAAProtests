05ca5044cb3d3abfdcacb848e547260538817403
Shivanku Kumar <kshivanku@gmail.com>
2020-01-05 11:21:26 -0800

    resuming animation on all bubbles

--- client/src/components/MapLayer.js
+++ client/src/components/MapLayer.js
@@ -68,7 +68,7 @@ export function MapLayer(props) {
                                     lineHeight: `calc(1rem + 0.2 * ${String(videoData[city].videos.length)}rem)`
                                 }}
                                 initial = {{scale: 1}}
-                                animate= {city=='Delhi' ? {scale: 1.2}: {scale:1}}
+                                animate= {{scale: 1.2}}
                                 transition = {{
                                     yoyo: Infinity,
                                     ease: 'easeOut',
